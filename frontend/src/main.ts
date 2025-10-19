2a5b86cfd998a9876c4fa055195cee8229a32411
Fofone9 <mr.paa2004@gmail.com>
2025-10-19 08:42:18 +0300

    добавит element-plus

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
@@ -8,6 +9,7 @@ import axios from 'axios'
 const app = createApp(App)
 
 // app.use(axios)
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
--- frontend/src/pages/PageApp.vue
+++ frontend/src/pages/PageApp.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-
-import Navbar from "@/models/navigation/Navbar.vue";
+import Navbar from '@/models/navigation/Navbar.vue'
 </script>
 
 <template>
-  <Navbar/>
-  <RouterView/>
+  <div class="flex flex-col gap-1">
+    <div class="flex-end">
+      <span>Фисунов Антон</span>
+      <ElButton>Выход</ElButton>
+    </div>
+
+    <Navbar />
+  </div>
+  <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
